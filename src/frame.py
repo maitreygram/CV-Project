5f2bc28193192824c104531f7ff61d072f46d3a6
Maitrey Gramopadhye <maitreygramo12@gmail.com>
2019-05-04 18:07:20 +0530

    ransac

---
+++ src/frame.py
@@ -0,0 +1,79 @@
+import numpy as np 
+import cv2
+from matplotlib import pyplot as plt
+from skimage.measure import ransac
+
+class Frame(object):
+	"""docstring for Frame"""
+	def __init__(self, frame):
+		super(Frame, self).__init__()
+		self.image = frame
+		(self.X, self.Y, self.Z) = frame.shape
+		self.KeyPts = None
+		self.des = None
+
+def getFeatures(frame):
+	orb = cv2.ORB_create()
+
+	frame_gray = cv2.cvtColor(frame.image, cv2.COLOR_BGR2GRAY)
+	corners = cv2.goodFeaturesToTrack(frame_gray,maxCorners=300,qualityLevel=0.01,minDistance=10)
+
+	KeyPts = []
+	for i in corners:
+		u,v = i.ravel()
+		KeyPts.append(cv2.KeyPoint(u,v, _size=20))
+	
+	frame.KeyPts, frame.des = orb.compute(frame.image, KeyPts)
+
+	# frame.KeyPts = [kp.pt for kp in KeyPts]
+	# frame.des = des
+	return
+
+def matchFeatures(F1, F2):
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+	
+	matches = bf.knnMatch(F1.des,F2.des, k=2)
+
+	temp_matches = []
+	for m,n in matches:
+		if m.distance < 0.75*n.distance and m.distance < 32:
+			temp_matches.append(m)
+
+
+	pt1 = []
+	pt2 = []
+	good_matches = []
+	for m in temp_matches:
+		if m.queryIdx not in pt1 and m.trainIdx not in pt2:
+			pt1.append(m.queryIdx)
+			pt2.append(m.trainIdx)
+			good_matches.append([F1.KeyPts[m.queryIdx],F2.KeyPts[m.trainIdx]])
+
+	# print len(good_matches), len(matches), len(idx1), len(idx2)
+	assert(len(good_matches) >= 8)
+
+	pt1 = np.array(pt1)
+	pt2 = np.array(pt2)
+	good_matches = np.array(good_matches)
+
+	print good_matches.shape
+
+	model, inliers = ransac((good_matches[:, 0], good_matches[:, 1]),
+                          	EssentialMatrixTransform,
+                          	min_samples=8,
+                          	residual_threshold=RANSAC_RESIDUAL_THRES,
+							max_trials=RANSAC_MAX_TRIALS)
+
+	return pt1[inliers], pt2[inliers], len(good_matches), fundamentalToRt(model.params)
+
+	# matches = sorted(matches, key = lambda x:x.distance)
+	# img3 = F1.image
+
+	# for m,n in matches:
+	# 	print i, m.distance, n.distance
+	# 	i = i + 1
+
+	# cv2.drawMatches(F1.image,F1.KeyPts,F2.image,F2.KeyPts,matches[:10], outImg=img3, flags=2)
+	# plt.imshow(img3),plt.show()
+	# cv2.waitKey(0)
+	return
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import pygame
+from frame import *
 
 X = 500
 Y = 500
@@ -20,8 +21,8 @@ def process_frame(frame):
 	disp = pygame.surfarray.make_surface(frame)
 	disp = pygame.transform.flip(disp, True, False)
 	for i in corners:
-	    x,y = i.ravel()
-	    pygame.draw.circle(disp,(255,0,0),(x,y),2)
+		x,y = i.ravel()
+		pygame.draw.circle(disp,(255,0,0),(x,y),2)
 
 	
 	
@@ -33,17 +34,48 @@ def process_frame(frame):
 
 	return
 	
-cap = cv2.VideoCapture('initial_key_frame.mp4')  
-frame_init = []
+cap = cv2.VideoCapture('../initial_key_frame.mp4')  
+
+# KeyFrames = []
+current_frames = []
 while cap.isOpened():
-    
-    ret, frame = cap.read()
-    if (ret == False):
-    	break
-    frame_init.append(frame)
-    
-    #process_frame(frame)
-    
+	
+	ret, frame = cap.read()
+	if (ret == False):
+		break
+
+	frame = cv2.resize(frame, (X, Y))
+	frame = Frame(frame)
+	current_frames.append(frame)
+
+	getFeatures(frame)
+
+	print len(current_frames)
+	if len(current_frames) > 1:
+		F1, F2, numMatched, R = matchFeatures(current_frames[-1], current_frames[-2])
+		exit(0)
+
+	# kp = [p.pt for p in frame.KeyPts]
+	# f = np.rot90(frame.image)
+	# corners = kp
+	# corners = np.int0(corners)
+
+	# disp = pygame.surfarray.make_surface(f)
+	# disp = pygame.transform.flip(disp, True, False)
+	# for i in corners:
+	# 	x,y = i.ravel()
+	# 	pygame.draw.circle(disp,(255,0,0),(x,y),2)
+
+	# screen.blit(disp, (0,0))
+	# pygame.display.update()
+	# pygame.display.flip()
+	
+	# cv2.waitKey(25)
+	
+
+	
+	# process_frame(frame)
+	
 
 # When everything done, release the capture
 print('a')
@@ -56,8 +88,8 @@ for frame in frame_init:
 	corners = cv2.goodFeaturesToTrack(frame_1,300,0.01,10)
 	keypoint_object = []
 	for i in corners:
-	    u,v = i.ravel()
-	    keypoint_object.append(cv2.KeyPoint(u,v, _size=20))
+		u,v = i.ravel()
+		keypoint_object.append(cv2.KeyPoint(u,v, _size=20))
 
 	kp, des = orb.compute(frame, keypoint_object)
 	# Extraction and 
